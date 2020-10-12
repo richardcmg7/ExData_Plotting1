d06b754717d1a84c8443d3567c8df8c07953e23a
Richard Saavedra <richardcmg7@gmail.com>
2020-10-11 19:59:18 -0500

    plot 4

--- plot4.R
+++ plot4.R
@@ -45,15 +45,15 @@ with(use_data, {
   plot(Global_active_power~Datetime, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="")
   plot(Voltage~Datetime, type="l", 
-       ylab="Voltage (volt)", xlab="")
+       ylab="Voltage (volt)", xlab="datetime")
   plot(Sub_metering_1~Datetime, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex= .5)
   plot(Global_reactive_power~Datetime, type="l", 
-       ylab="Global Rective Power (kilowatts)",xlab="")
+       ylab="Global Rective Power (kilowatts)",xlab="datetime")
   
 })
 
